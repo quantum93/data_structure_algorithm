38a44b61fc43538681ac70506ef41f1ff1ea8228
Tae Lee <taebumlee@gmail.com>
2020-09-14 14:45:15 -0700

    Update find_the_symmetric_difference.js

--- find_the_symmetric_difference.js
+++ find_the_symmetric_difference.js
@@ -1,20 +1,19 @@
 function sym(...args) {
   let uniqueArr = args.map(arr => [...new Set(arr)]);
-  while(uniqueArr.length >= 1) {
-    let difference = uniqueArr[0]
-                          .filter(x => !uniqueArr[1].includes(x))
-                          .concat(uniqueArr[1].filter(x => !uniqueArr[0].includes(x)));
-
+  for(let i = 0; i < args.length-1; i++) {
+    let difference = uniqueArr[0].filter(x => !uniqueArr[1].includes(x))
+                                 .concat(uniqueArr[1].filter(x => !uniqueArr[0].includes(x)));
+    uniqueArr = [difference, ...uniqueArr.slice(2)];
   }
-  return difference;
+  return uniqueArr.flat().sort();
 }
 
 console.log(sym([1, 2, 3], [5, 2, 1, 4]));
 console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]));
 console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
 
-/* let arr1 = args[0].filter(e => !args[1].includes(e));
-let arr2 = args[1].filter(e => !args[0].includes(e));
-let result = args.slice(2).concat(arr1, arr2).sort();
-return [...new Set(result)]; */
 
+/*     let difference = uniqueArr[0]
+                          .filter(x => !uniqueArr[1].includes(x))
+                          .concat(uniqueArr[1].filter(x => !uniqueArr[0].includes(x)));
+    uniqueArr = result.push(difference); */
