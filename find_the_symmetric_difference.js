972904b66c2c5249561ccbf8b8cab5a48d59a6ae
Tae Lee <taebumlee@gmail.com>
2020-09-14 19:36:16 -0700

    Update find_the_symmetric_difference.js

--- find_the_symmetric_difference.js
+++ find_the_symmetric_difference.js
@@ -67,11 +67,29 @@ console.log(sym3([1, 2, 3], [5, 2, 1, 4]));
 console.log(sym3([1, 2, 3, 3], [5, 2, 1, 4]));
 console.log(sym3([1, 2, 5], [2, 3, 5], [3, 4, 5]));
 
-/** 가장 깔끔한 접근 */
+/** 가장 깔끔한 접근 
+ * reduce를 이용해서 복잡한 로직을 최소화.
+ * reduce(diff)는 diff함수를 모든 배열에 적용해서 결과를 반환.
+ * diff 함수는, 
+ * arr1은 배열 ...arr1은 배열의 원소를 spread하고 여기에 filter를 
+ * 적용 결과로 나온 원소들을 하나의 배열로 합하는 로직. 이 함수 자체로 
+ * 두 배열의 symmetric difference set이 구해진다. 
+ * 
+ * args.reduce(diff)하면 2D 배열의 각 원소배열에 대해서 diff를 작동. 
+ * 즉, diff함수가 reduce()에서 작동하는 callback함수가 된다. 
+ * 그 결과로 얻어지는 배열을 Set으로 중복처리 해서 다시 배열형태로 전환
+ * 이렇게 하는것이 
+ * let difference = uniqueArr[0].filter(x => !uniqueArr[1].includes(x))
+                                 .concat(uniqueArr[1].filter(x => !uniqueArr[0].includes(x)));
+    uniqueArr = [difference, ...uniqueArr.slice(2)];
+
+ *의 복잡성을 아주 많이 줄여준다.
+*/
+
 const diff = (arr1, arr2) => [
   ...arr1.filter(e => !arr2.includes(e)), 
   ...arr2.filter(e => !arr1.includes(e)) 
-];
+  ];
 
 const sym4 = (...args) => [...new Set(args.reduce(diff))];
 
