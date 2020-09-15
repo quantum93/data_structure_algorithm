9f50c98fc825f77b561ec41e6f47fae32f58d8e8
Tae Lee <taebumlee@gmail.com>
2020-09-15 10:44:57 -0700

    Update inventory_update.js

--- inventory_update.js
+++ inventory_update.js
@@ -1,15 +1,26 @@
 var curInv = [[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5,"Microphone"]];
 var newInv = [[2,"Hair Pin"],[3,"Half-Eaten Apple"],[67,"Bowling Ball"],[7,"Toothpaste"]];
 
-function updateInventory(arr1, arr2) {
-  let newItem = arr2.map(e => e[1]);
-  let curItem = arr1.map(e => e[1]);
-  for(let i = 0; i < newItem.length; i++) {
-    if(curItem.includes(newItem[i])) {
-      console.log(newItem[i]);
-    }
-  }
+function updateInventory(curInv, newInv) {
+  let updateInv = curInv.filter(e => newInv.map(e => e[1]).includes(e[1]));
+  let addInv = newInv.filter(e => !curInv.map(e => e[1]).includes(e[1]));
+
+  return curInv.concat(addInv)
+               .sort((a,b) => {
+                 if(a[1] < b[1]) {return -1}; //something negative if first argument is less than second (should be placed before the second in resulting array)
+                 if(a[1] > b[1]) {return 1}; // something positive if first argument is greater (should be placed after second one)
+                 return 0; // 0 if those two elements are equal.
+                });
+
 }
 
 console.log(updateInventory(curInv, newInv));
+
 //arr1.filter(e => newItem.includes(e[1]))
+
+/*   
+for(let i = 0; i < newItem.length; i++) {
+  if(curItem.includes(newItem[i])) {
+    console.log(newItem[i]);
+  }
+} */ 
