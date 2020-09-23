bd3338f1f543d59f2e824e5661159379f5d512ba
Tae Lee <taebumlee@gmail.com>
2020-09-23 12:53:55 -0700

    Update pairwise.js

--- pairwise.js
+++ pairwise.js
@@ -10,4 +10,7 @@ function pairwise(arr, arg) {
   return result;
 }
 
-console.log(pairwise([1,4,2,3,0,5], 7));
+console.log(pairwise([1,4,2,3,0,5], 7)); // 11
+console.log(pairwise([1,1,1], 2)); // 1
+console.log(pairwise([1,1,2], 3)); // 2
+console.log(pairwise([0,0,0,0,1,1], 1)); // 10
