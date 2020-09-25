69a988208af8bffe325f9c3e9f9f89afdfc7fc71
Tae Lee <taebumlee@gmail.com>
2020-09-25 16:49:27 -0700

    Create bubble_sort.js

---
+++ bubble_sort.js
@@ -0,0 +1,7 @@
+function bubbleSort(array) {
+  // change code below this line
+  return array;
+  // change code above this line
+}
+
+console.log(bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
