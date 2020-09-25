a9f6e9add4d126de51a5b3798ab20c1d7d36d46d
Tae Lee <taebumlee@gmail.com>
2020-09-25 13:45:59 -0700

    Update pairwise.js

--- pairwise.js
+++ pairwise.js
@@ -1,18 +1,18 @@
 function pairwise(arr, arg) {
-  let result = 0;
+  let result = [];
   for(let i = 0; i < arr.length-1; i++) {
     for(let j = i+1; j < arr.length; j++) {
       //console.log(i, j);
-      if(arr[i] + arr[j] === arg) {
-        console.log(i, j);
-        result += i+j;
+      if(arr[i] + arr[j] === arg && !result.includes(i) && !result.includes(j) ) {
+        result.push(i,j);
       }
     }
   }
-  return result;
+  return result.reduce((a,b) => a + b, 0);
 }
 
 console.log(pairwise([1,4,2,3,0,5], 7)); // 11
 console.log(pairwise([1,1,1], 2)); // 1
 console.log(pairwise([1,1,2], 3)); // 2
 console.log(pairwise([0,0,0,0,1,1], 1)); // 10
+console.log(pairwise([], 100)) // 0
