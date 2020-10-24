875dcecf2dce7075a9e32f4614ba496081c152a1
Tae Lee <taebumlee@gmail.com>
2020-10-24 14:56:38 -0700

    Update primeFactorization.js

--- primeFactorization.js
+++ primeFactorization.js
@@ -1,17 +1,21 @@
 const primeFactor = n => {
+  let result = [];
+
   while (n % 2 == 0) {
-    console.log(2);
+    result.push(2);
     n = n / 2;
   }
 
   for(let i = 3; i <= n**.5; i += 2) {
     while(n%i == 0) {
-      console.log(i);
+      result.push(i);
       n = n / i;
     }
   }
 
-  if(n > 2) console.log(n);
+  if(n > 2) result.push(n);
+  
+  return result;
 }
 
 console.log(primeFactor(10));
