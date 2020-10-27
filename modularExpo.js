e8ee0cc44067f871f58ac61ad7ef5d552fda051a
Tae Lee <taebumlee@gmail.com>
2020-10-27 10:12:30 -0700

    Create modularExpo.js

---
+++ modularExpo.js
@@ -0,0 +1,11 @@
+const modularExpo = (base, exponent, modulus) => {
+  if(modulus == 0) return 0;
+  let value = 1;
+  for(let i = 0; i < exponent; i++) {
+    value = (value * base) % modulus;
+  }
+  return value;
+}
+
+console.log(modularExpo(4,3,5));
+console.log(modularExpo(6*10**77,27,497));
