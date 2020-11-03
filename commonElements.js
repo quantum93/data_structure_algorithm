c0a587426a4d6639f12fa7e55e6ba23a1a0e218f
Tae Lee <taebumlee@gmail.com>
2020-11-03 13:11:51 -0800

    Update commonElements.js

--- commonElements.js
+++ commonElements.js
@@ -1,7 +1,21 @@
 const commonElements = (kArray) => {
-  for(let i = 0; i < kArray.length; i++) {
-    console.log(kArray[i]);
+  let hashmap = {}, last, answer = [];
+  for(let i = 0, kArrLen = kArray.length; i < kArrLen; i++) {
+    let currentArr = kArray[i];
+    last = null;
+    for(let j = 0, currentArrLen = currentArr.length; j < currentArrLen; j++) {
+      let currentEl = currentArr[j];
+      if(last != currentEl) {
+        return (!hashmap[currentEl]) ? hashmap[currentEl] = 1: hashmap[currentEl]++;
+      }
+      last = currentEl;
+    }
   }
+  
+  for(let prop in hashmap) {
+    if(hashmap[prop] == kArray.length) answer.push(parseInt(prop));
+  }
+  return answer;
 }
 
-console.log(commonElements([1,2,3,4],[1,2,3,],[1,2]));
+console.log(commonElements([1,2,3,],[1,2,3,4],[1,2]));
